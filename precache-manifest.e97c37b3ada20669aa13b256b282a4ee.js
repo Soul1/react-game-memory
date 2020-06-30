7301243dad067926b456b9e81484c1645b4be67e
Soul1 <darkwaud@gmail.com>
2020-06-30 05:57:31 +0300

    Updates

---
+++ precache-manifest.e97c37b3ada20669aa13b256b282a4ee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1216b202bc65d92d21781cd95b9cd37c",
+    "url": "/react-game-memory/index.html"
+  },
+  {
+    "revision": "81b2f848bbbdfbeb6236",
+    "url": "/react-game-memory/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "9dedaa747b1b0b8c7316",
+    "url": "/react-game-memory/static/css/main.c5077bb0.chunk.css"
+  },
+  {
+    "revision": "81b2f848bbbdfbeb6236",
+    "url": "/react-game-memory/static/js/2.4e80c312.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-game-memory/static/js/2.4e80c312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dedaa747b1b0b8c7316",
+    "url": "/react-game-memory/static/js/main.9e22429a.chunk.js"
+  },
+  {
+    "revision": "07a4cb14005dccd30e77",
+    "url": "/react-game-memory/static/js/runtime-main.296c49f7.js"
+  },
+  {
+    "revision": "33643a13c319df77973db5040f1a966c",
+    "url": "/react-game-memory/static/media/back.33643a13.svg"
+  },
+  {
+    "revision": "0e87203b3353752fd75a23df6735443b",
+    "url": "/react-game-memory/static/media/canada.0e87203b.svg"
+  },
+  {
+    "revision": "6a686ea538818ad09471ba3b4cf03742",
+    "url": "/react-game-memory/static/media/china.6a686ea5.svg"
+  },
+  {
+    "revision": "a2397d3153ff13c504889e2e9f6bd42d",
+    "url": "/react-game-memory/static/media/france.a2397d31.svg"
+  },
+  {
+    "revision": "599e2907949c2fdc47a7444cd8b024a6",
+    "url": "/react-game-memory/static/media/germany.599e2907.svg"
+  },
+  {
+    "revision": "15dd512f4ac2ead07046ca7a3dc32c33",
+    "url": "/react-game-memory/static/media/italy.15dd512f.svg"
+  },
+  {
+    "revision": "c1b811d08bf51a826966c17605e724ad",
+    "url": "/react-game-memory/static/media/russia.c1b811d0.svg"
+  },
+  {
+    "revision": "3e0210d2e9dd9806ccc404f08f5f910c",
+    "url": "/react-game-memory/static/media/uk.3e0210d2.svg"
+  },
+  {
+    "revision": "4a52512e9fa2b4741f371fee88abc9d9",
+    "url": "/react-game-memory/static/media/usa.4a52512e.svg"
+  }
+]);
